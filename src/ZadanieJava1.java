fffe46f8eb46ea36ae347dbd1a97d5ba3f9c9ee7
miki1989 <mikolajheller@gmail.com>
2018-07-02 14:21:22 +0200

    Third commit with add calc.

--- src/ZadanieJava1.java
+++ src/ZadanieJava1.java
@@ -5,5 +5,13 @@ public class ZadanieJava1 {
         System.out.println("programowania");
         System.out.println("i");
         System.out.println("commitowania");
+
+        System.out.println();
+        System.out.println("Próba obliczeń");
+        int x = 8;
+        int y = 10;
+        int z;
+        z = x*y;
+        System.out.println(z);
     }
 }
